124ca6a1b215b77ce20559e97dc302a432e77a6d
acrosspg@gmail.com <acrosspg@gmail.com@950d0675-1da2-755d-7c49-59f284cbbb14>
2013-11-15 19:21:44 +0000

    iframe

--- acrosspg-books.js
+++ acrosspg-books.js
@@ -6,6 +6,7 @@ images[1] = "<a href = 'http://www.flipkart.com/roams-review-all-medical-subject
 images[2] = "<a href = 'http://www.flipkart.com/comprehensive-guide-combined-medical-services-upsc-simplified-2nd/p/itmdjg92pettetth?pid=9788131233191&affid=acrosspgya' target='_blank'><img src='https://lh5.googleusercontent.com/-FkCYBgiW14w/UoP_FuJeO6I/AAAAAAAABCU/giKcejcr8-Y/w300-h250-no/upsc-2ed.jpg' alt='UPSC CMS Solved'></a>";
 images[3] = "<a href = 'http://www.flipkart.com/dnb-cet-review-primary-post-diploma-2013-supplement-1st/p/itmdqd7arbhw6qux?pid=9788189477202&affid=acrosspgya' target='_blank'><img src='https://lh4.googleusercontent.com/s5gLD3gZMWd7IdVaOJdcGRGZeIg9H3KZ0yof5Y1-EJg=w300-h250-no' alt='DNB CET Review 2013 Supplement'></a>";
 images[4] = "<a href = 'http://www.flipkart.com/sure-success-magic-7th/p/itmdq4677xzksya5?pid=9789351520009&&affid=acrosspgya' target='_blank'><img src='https://lh5.googleusercontent.com/-qr-8BSmpVCE/UoTOhctYQYI/AAAAAAAABCw/HcSl1UnrY8Y/w300-h250-no/sure+success+magic+7ed.jpg' alt='Sure Success Magic'></a>";
+images[5] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138454324291031221' frameborder=0 height=250 width=300></iframe>";
 
 index = Math.floor(Math.random() * images.length);
 document.write(images[index]);
