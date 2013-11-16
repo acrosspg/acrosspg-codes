36807c75c9ab81ca999bf571665b7b9861eb8d88
acrosspg@gmail.com <acrosspg@gmail.com@950d0675-1da2-755d-7c49-59f284cbbb14>
2013-11-16 00:04:08 +0000

    tiny test

--- acrosspg-books.js
+++ acrosspg-books.js
@@ -1,7 +1,7 @@
 var images = [], 
 index = 0;
 
-images[0] = "<a href = 'http://www.flipkart.com/dnb-cet-review-primary-post-diploma-volume-1-2-3rd/p/itmdmfu2wk2gzwpc?pid=9788192022420&affid=acrosspgya' target='_blank'><img src='https://lh4.googleusercontent.com/-O9LWjrJlTYY/UoPrEUNmPPI/AAAAAAAABBk/tY49dRHcr_Q/w300-h250-no/dnb-cet-review-3ed.jpg' alt='DNB CET Review 3rr Edition'></a>";
+images[0] = "<a href = 'http://www.flipkart.com/dnb-cet-review-primary-post-diploma-volume-1-2-3rd/p/itmdmfu2wk2gzwpc?pid=9788192022420&affid=acrosspgya' target='_blank'><img src='http://tiny.cc/ur7l6w' alt='DNB CET Review 3rr Edition'></a>";
 images[1] = "<a href = 'http://www.flipkart.com/roams-review-all-medical-subjects-10th/p/itmdnnanqtjdsatt?pid=9788189926793&affid=acrosspgya' target='_blank'><img src='https://lh3.googleusercontent.com/-Ldb__mwvXeE/UoP_FSZVe9I/AAAAAAAABCQ/wXtuINWb3s8/w300-h250-no/roams-10ed.jpg' alt='ROAMS 10th Edition'></a>";
 images[2] = "<a href = 'http://www.flipkart.com/comprehensive-guide-combined-medical-services-upsc-simplified-2nd/p/itmdjg92pettetth?pid=9788131233191&affid=acrosspgya' target='_blank'><img src='https://lh5.googleusercontent.com/-FkCYBgiW14w/UoP_FuJeO6I/AAAAAAAABCU/giKcejcr8-Y/w300-h250-no/upsc-2ed.jpg' alt='UPSC CMS Solved'></a>";
 images[3] = "<a href = 'http://www.flipkart.com/dnb-cet-review-primary-post-diploma-2013-supplement-1st/p/itmdqd7arbhw6qux?pid=9788189477202&affid=acrosspgya' target='_blank'><img src='https://lh4.googleusercontent.com/s5gLD3gZMWd7IdVaOJdcGRGZeIg9H3KZ0yof5Y1-EJg=w300-h250-no' alt='DNB CET Review 2013 Supplement'></a>";
